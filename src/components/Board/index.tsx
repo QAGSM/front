c69333446f78b1c6b689b2288fc0ae5bb38aa0b5
TaerangLee <s22051@gsm.hs.kr>
2024-03-04 01:37:56 +0900

    Boardstyle

---
+++ src/components/Board/index.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import * as S from "./style";
+
+const Board = () => {
+  return (
+    <S.BoardWrapper>
+      <S.BoardContainer>asd</S.BoardContainer>
+    </S.BoardWrapper>
+  );
+};
+
+export default Board;
---
+++ src/components/Board/style.ts
@@ -0,0 +1,13 @@
+import { theme } from "@/style";
+import styled from "@emotion/styled";
+
+export const BoardWrapper = styled.div`
+  width: 100%;
+  height: 360px;
+  padding: 30px;
+  border-radius: 10px;
+`;
+
+export const BoardContainer = styled.div`
+  padding: 30px;
+`;
