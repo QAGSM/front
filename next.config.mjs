f223cf06d0ea7892f0194a39b16ce4e80c36cad3
Lee-Seungje <moondgod@gmail.com>
2024-03-04 03:28:08 +0900

    Set api

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  rewrites: async () => [
+    {
+      source: "/api/:path*",
+      destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+    },
+  ],
+};
 
 export default nextConfig;
