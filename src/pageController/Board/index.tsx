3cd14fc3f53ae1d848607e6fb464473077780a25
TaerangLee <s22051@gsm.hs.kr>
2024-03-04 02:52:01 +0900

    Update postBoard, alert toastify

--- src/pageController/Board/index.tsx
+++ src/pageController/Board/index.tsx
@@ -3,13 +3,25 @@
 import { Input } from "@/components";
 import * as S from "./style";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const Board = () => {
   const [title, setTitle] = useState<string>("");
   const [content, setContent] = useState<string>("");
 
+  const { push } = useRouter();
+
   const TITLELENGTH = 50 as const;
   const CONTENTLENGTH = 500 as const;
+
+  const onSubmit = () => {
+    if (title && content) {
+      console.log("성공");
+      push("/");
+    } else {
+      console.log("실패");
+    }
+  };
 
   return (
     <S.BoardWrapper>
@@ -21,14 +33,18 @@ const Board = () => {
         <S.Input
           placeholder="제목을 작성해주세요. (최대 50글자)"
           maxLength={TITLELENGTH}
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
         />
 
         <S.BoardTextArea
           placeholder="내용을 작성해주세요. (최대 500글자)"
           maxLength={CONTENTLENGTH}
+          onChange={(e) => setContent(e.target.value)}
+          value={content}
         />
 
-        <S.SubmitButton>작성하기</S.SubmitButton>
+        <S.SubmitButton onClick={onSubmit}>작성하기</S.SubmitButton>
       </S.BoardContainer>
     </S.BoardWrapper>
   );
