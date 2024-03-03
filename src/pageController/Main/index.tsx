8330b2f7a030f2b4582efaa9103052660745d917
TaerangLee <s22051@gsm.hs.kr>
2024-03-04 04:58:55 +0900

    etst1

--- src/pageController/Main/index.tsx
+++ src/pageController/Main/index.tsx
@@ -3,6 +3,8 @@
 import * as S from "./style";
 import { useRouter } from "next/navigation";
 import { ListItem } from "@/components";
+import { axiosInstance } from "@/api";
+import { useEffect, useState } from "react";
 
 const DummyData = [
   {
@@ -55,6 +57,18 @@ const DummyData = [
 const Main = () => {
   const { push } = useRouter();
 
+  useEffect(() => {
+    axiosInstance
+      .get("/questions", {})
+      .then((response) => {
+        const fetchedData = response.data;
+        console.log(fetchedData);
+      })
+      .catch((error) => {
+        console.error("데이터를 불러오는 중 오류 발생:", error);
+      });
+  }, []);
+
   return (
     <S.MainWrapper>
       <S.Title>QA</S.Title>
@@ -63,8 +77,8 @@ const Main = () => {
           게시글 작성하기
         </S.BoardButton>
         <S.ListContainer>
-          {DummyData.map((item, index) => (
-            <ListItem key={index}>{item.title}</ListItem>
+          {fetchedData?.map((item, index) => (
+            <ListItem key={index}>{item}</ListItem>
           ))}
         </S.ListContainer>
       </S.BoardButtonContainer>
