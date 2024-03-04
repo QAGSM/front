d37886d75816f368b0a02ecd010e6a35535b2b0d
Lee-Seungje <moondgod@gmail.com>
2024-03-04 20:14:52 +0900

    Update

--- src/pageController/Main/index.tsx
+++ src/pageController/Main/index.tsx
@@ -16,7 +16,7 @@ const Main = () => {
       .get("/questions", {})
       .then((response) => {
         const fetchedData = response.data;
-        setFetchedData(fetchedData);
+        setFetchedData(fetchedData.reverse());
       })
       .catch((error) => {
         console.error("데이터를 불러오는 중 오류 발생:", error);
